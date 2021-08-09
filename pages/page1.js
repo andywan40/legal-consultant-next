555875e70e68efc6303d3c12f0f25cd918358274
andywan40 <andywan40@gmail.com>
2021-08-09 18:07:49 +0800

    update styles

--- pages/page1.js
+++ pages/page1.js
@@ -13,11 +13,11 @@ export default function page1() {
       <div className="grid grid-cols-12 max-h-screen w-full py-20 ">
         <div className="col-start-3 col-span-6 border-r border-gray-400">
           <div className="flex justify-center border-b border-gray-400 pb-6 mx-10 h-8vh">
-            <button className="border border-gray-400 rounded-lg bg-white hover:bg-gray-100 text-black py-3 px-10 mx-3 rounded-full">
-              判決
+            <button className="border border-gray-400 rounded-lg bg-white hover:bg-gray-100 text-black py-5 px-10 mx-3 rounded-full flex items-center">
+              <span>判決</span>
             </button>
-            <button className="border border-gray-400 rounded-lg bg-white hover:bg-gray-100 text-black py-3 px-10 mx-3 rounded-full">
-              函釋
+            <button className="border border-gray-400 rounded-lg bg-white hover:bg-gray-100 text-black py-5 px-10 mx-3 rounded-full flex items-center">
+              <span>函釋</span>
             </button>
           </div>
           <div className="pb-6 mx-10 overflow-auto h-75vh">
@@ -29,7 +29,7 @@ export default function page1() {
         </div>
         <div className="col-start-9 col-span-3">
           <div className="flex fkex-col items-center justify-center border-b border-gray-400 pb-6 h-8vh"></div>
-          <div className="p-5 mx-10 overflow-auto h-75vh">
+          <div className="p-5 overflow-auto h-75vh">
             <h1 className="text-blue-800 text-lg font-bold">相關條文</h1>
             <div className="mt-2">
               {accordions &&
